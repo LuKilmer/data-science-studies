62df1b4fe897e45aa58cd9344e13ed4bd167984d
nonluck <luiskilmerluk1221@gmail.com>
2023-12-29 21:40:01 -0300

    fix:remove this shit

--- P&E/asd.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-idade = [16, 17, 18, 16, 17, 16, 18, 17, 16, 18,
-         17, 16, 18, 16, 17, 18, 16, 17, 16, 18,
-         17, 18, 16, 17, 18, 16, 17, 16, 18, 17]
-
-contador = Counter(idade)
-print(list(contador.values()))
-print(list(contador.keys()))
